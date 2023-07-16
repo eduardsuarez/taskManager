f79531da45857b44936f78854d3a373c794b6f6c
Eduard Suárez <suareze205@gmail.com>
2023-07-16 15:42:59 -0500

    Elimino los archivos del proyecto anterior

--- src/main/java/com/dev/taskManager/crudRepository/commentCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dev.taskManager.crudRepository;
-
-import com.dev.taskManager.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface commentCrudRepository extends JpaRepository<Comment, Integer> {
-
-}
--- src/main/java/com/dev/taskManager/crudRepository/labelCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dev.taskManager.crudRepository;
-
-import com.dev.taskManager.entity.Label;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface labelCrudRepository extends JpaRepository<Label,Integer> {
-}
